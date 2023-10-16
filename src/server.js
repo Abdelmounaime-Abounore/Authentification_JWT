5f120c742df994da5b4361c70faea1363e44c240
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-10-16 15:24:24 +0100

    add nodemon and edit in db.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const express = require("express")
-// const dotenv = require("dotenv")
+const dotenv = require("dotenv")
 const db = require("../config/db.js")
 
 const app = express()
 
 app.use(express.json())
 
-// dotenv.config()
+dotenv.config()
 
 
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 8000
 db()
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`) )
