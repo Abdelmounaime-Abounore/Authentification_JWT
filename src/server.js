236ecb59717a66486db2c4257728602e9f9e8db8
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-10-16 14:35:53 +0100

    config .env and connect database

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+// const dotenv = require("dotenv")
+const db = require("../config/db.js")
+
+const app = express()
+
+app.use(express.json())
+
+// dotenv.config()
+
+
+
+const PORT = process.env.PORT 
+db()
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`) )
