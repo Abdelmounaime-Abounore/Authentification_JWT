3f3fea01ca500b82b79bc4dd0d45e0265159243f
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-10-17 17:55:47 +0100

    pushing

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require('../../controllers/authContoller')
 
 router.post("/register", authController.register)
+router.get('/verify/:token', authController.verifyEmail);
 
 module.exports = router
