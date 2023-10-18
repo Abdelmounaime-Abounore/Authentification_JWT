1b201b3146a1e0171ae3bade1fe653a2b532683a
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-10-18 14:18:36 +0100

    login & send email

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,8 +1,9 @@
-const User = require('../models/user');
+const User = require('../models/user')
 const mongoose = require("mongoose")
-const roleId = "652e4b682547cf7e2afe4045";
+const roleId = "652e4b682547cf7e2afe4045"
 const jwt = require('../utils/jwtToken')
-const sendEmail = require('../utils/sendEmail')
+
+const sendEmail = require("../utils/sendEmail")
 
 const register = async (req, res) => {
     try {
@@ -30,6 +31,53 @@ const register = async (req, res) => {
     }
 };
 
+const login = async(req, res) => {
+
+    const {email , password} = req.body;
+
+    const user = await User.findOne({email})
+
+    if(user && password == user.password)
+    {
+        const verificationToken = jwt.jwtToken.generate(user._id , '10m')
+
+        if(!user.isVerified){
+            const verificationLink = `${process.env.BASE_URL}/api/auth/users/verify/${verificationToken}`;
+            await sendEmail.sendEmail(user.email, "Email Verification", verificationLink);
+            res.json({ message : "please check your email "})
+        }
+
+        }else{
+            res.status(401).json({ message: 'Info Invalide' });
+        }
+} 
+
+const emailVerification = async(req, res) => {
+    const { token } = req.params;
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (decodedToken.id) {
+            // Find user and mark as verified
+            const user = await User.findById(decodedToken.id);
+
+            if (user) {
+                user.isVerified = true;
+                await user.save();
+                return res.status(200).json({ message: 'Email verified successfully.' });
+            }
+        }
+        return res.status(400).json({ message: 'Invalid token for email verification.' });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+}
+
 module.exports = {
-    register
+    register,
+    login,
+    emailVerification
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const authController = require('../../controllers/authContoller')
 
 router.post("/register", authController.register)
-// router.get('/verify/:token', authController.verifyEmail);
+router.post("/login", authController.login)
+router.get("/email-verification/:token", authController.emailVerification)
 
 module.exports = router
